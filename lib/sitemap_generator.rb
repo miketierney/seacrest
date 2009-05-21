7ae10df10c9bbbb182b41bf78824715c9976228e
Matthew Anderson <manderson@onehub.com>
2009-05-20 19:16:03 -0700

    Sitemap.xml gets ignored

--- lib/sitemap_generator.rb
+++ lib/sitemap_generator.rb
@@ -11,7 +11,7 @@ class SitemapGenerator
     Find.find origin do |path|
       path =~ /^#{origin}\/(.*)/
       unless File.directory? path
-        @pages << $1
+        @pages << $1 unless $1 == 'sitemap.xml'
       end
     end
     @pages
