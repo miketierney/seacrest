ff9bd27bcb05f9b51dd5a59e122c4d140f46b282
Mike Tierney <mike@panpainter.com>
2009-05-29 16:25:28 -0700

    Adding in the basis for the html collector.

---
+++ lib/seacrest/collectors/html_collector.rb
@@ -0,0 +1,16 @@
+require 'csspool'
+require 'seacrest/collectors'
+
+module Seacrest
+  module Collectors
+    class HTMLCollector
+      def initialize
+        # ...
+      end
+      
+      def parse
+        # ...
+      end
+    end
+  end
+end
