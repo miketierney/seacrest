e9cc20eb3189ecf6648eeb6175ce6b706274a5b6
Mike Tierney <mike@panpainter.com>
2009-06-03 20:18:51 -0700

    Parses the HTML file and returns an array of unused CSS selectors.  Yay nokogiri!

--- lib/seacrest/collectors/html_collector.rb
+++ lib/seacrest/collectors/html_collector.rb
@@ -3,9 +3,22 @@ require 'seacrest/collectors'
 
 module Seacrest
   class HTMLCollector
+    attr_accessor :selectors, :unused_selectors
 
     def initialize
-      # ...
+      @selectors = Array.new # This array should be populated by the CSS Collector, since that's what we're checking against.
+      @unused_selectors = Array.new
+    end
+
+    def process file
+      doc = Nokogiri::HTML(open(file))
+
+      @selectors.each do |selector|
+        if doc.search(selector).empty?
+          next if @unused_selectors.include? selector
+          @unused_selectors << selector
+        end
+      end
     end
 
     def handles? file
--- test/collectors/test_html_collector.rb
+++ test/collectors/test_html_collector.rb
@@ -4,10 +4,23 @@ class TestHTMLCollector < Test::Unit::TestCase
   def setup
     @my_html = HTMLCollector.new
     @html_file = "#{ASSET_DIR}/csscrubber.html"
+    @selectors = ['body', 'h2', '.not_in_file', '.not_in_file']
   end
 
   # This test is mostly to get me up and running on a few different other fronts; just want to make sure that I can actually access my HTMLCollector class right now.
   def test_is_html
     assert @my_html.handles?(@html_file), "Detecting that the file is actually an HTML file."
+  end
+  
+  def test_process_html_doesnt_explode
+    assert @my_html.process(@html_file), "Doesn't blow up when I ask it to process a file"
+  end
+  
+  def test_unused_selectors_gets_populated
+    @my_html.selectors = @selectors
+    @my_html.process(@html_file)
+    actual = @my_html.unused_selectors
+    expected = ['.not_in_file']
+    assert_equal expected, actual
   end
 end
