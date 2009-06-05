5c72ac63b3d6353f8a56adcfdd4ae838753b33f0
Mike Tierney <mike@panpainter.com>
2009-06-05 07:36:03 -0700

    Getting the Scrubber to traverse the document tree and find all of the HTML and CSS files.  Feels a little redundant right now, but it's working for the moment.

--- lib/seacrest/csscrubber.rb
+++ lib/seacrest/csscrubber.rb
@@ -1,32 +1,49 @@
+require 'find'
 require 'csspool'
 
-# List of things to tend to:
-#
-# Add the ability to ignore known frameworks (960.gs, blueprint.css, etc) or the ability to include these if they are ignored by default
-#
 module Seacrest
   class CSScrubber
+    
+    attr_accessor :css_files, :html_files
 
-    def initialize(file)
-      @file = file
+    def initialize root
+      @root = root
+      # @file = file
       @css_files = Array.new
       @html_files = Array.new
+      
+      traverse
     end
     
-    def process_file
-      raise "The file you supplied, #{File.basename(@file)}, is not a valid file; it either may not exist, or it may have been a directory." unless File.exist?(@file)
-
-      if Collectors.can_handle? @file
-        css = Collectors::CSSCollector.new
-        css.process @file
-
-        @all_selectors = css.all_selectors
-        @selectors = css.unique_selectors
-        
-        html = Collectors::HTMLCollector.new
-        html.process @file
+    def traverse
+      Find.find @root do |path|
+        path =~ /^#{@root}\/\w*.(\w*)/ # only need to capture the extension, since that's what we're checking against, and we're storing the whole path in the array
+        unless File.directory? path
+          if $1 == "css"
+            @css_files << path
+          elsif $1 == "html"
+            @html_files << path
+          end
+        end
       end
     end
+    
+    def process_files
+      for file in @css_files
+        if Collectors.can_handle? @file
+          css = Collectors::CSSCollector.new
+          css.process file
 
+          @all_selectors = css.all_selectors
+          @selectors = css.unique_selectors
+        end
+      end
+      
+      for file in @html_files
+        html = Collectors::HTMLCollector.new
+        html.process file
+      end
+    end
+    
   end
 end
--- test/csscrubber/test_csscrubber.rb
+++ test/csscrubber/test_csscrubber.rb
@@ -4,18 +4,34 @@ class TestCSScrubber < Test::Unit::TestCase
   def setup
     @css = "#{ASSET_DIR}/csscrubber.css"
     @html = "#{ASSET_DIR}/csscrubber.html"
-    @scrubber = Seacrest::CSScrubber.new @css
+    @scrubber = Seacrest::CSScrubber.new ASSET_DIR
   end
 
   def test_asset_directory_exists # 'cause I created it, and I want to make sure that this actually works.  Will fail if the stuff doesn't exist.
     assert File.exist?(@html), "CSScrubber html file should exist."
   end
   
-  def test_should_warn_if_file_doesnt_exist
-    new_scrubber = CSScrubber.new "massive_fail.html"
-
-    assert_raise RuntimeError do
-      new_scrubber.process_file
-    end
+  # def test_should_warn_if_file_doesnt_exist
+  #   new_scrubber = CSScrubber.new "massive_fail.html"
+  # 
+  #   assert_raise RuntimeError do
+  #     new_scrubber.process_file
+  #   end
+  # end
+  
+  def test_stores_html_files_recursively_through_traverse
+    expected = [
+      "#{ASSET_DIR}/index.html",
+      "#{ASSET_DIR}/csscrubber.html"
+    ]
+    assert_equal expected, @scrubber.html_files
   end
+  
+  def test_stores_css_files_recursively_through_traverse
+    expected = [
+      "#{ASSET_DIR}/csscrubber.css"
+    ]
+    assert_equal expected, @scrubber.css_files
+  end
+  
 end
