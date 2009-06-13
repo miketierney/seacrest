6efb221f1ab3cbb1fd9ee6a2c506277f0c7366d7
Mike Tierney <mike@panpainter.com>
2009-06-12 22:38:12 -0700

    Re-adding the seacrest.rb file.

---
+++ lib/seacrest.rb
@@ -0,0 +1,9 @@
+require 'nokogiri'
+require 'seacrest/csscrubber'
+require 'seacrest/collectors'
+require 'seacrest/sitemap_generator'
+require 'seacrest/url_checker'
+
+module Seacrest
+  VERSION = '0.0.1'
+end
